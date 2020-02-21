063262ae80c8703b9b9624cfd9624648a9620665
Jeff Weiss <jweiss@enbala.com>
2020-02-21 15:50:00 +0100

    bump to 0.1.1

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Jira.Mixfile do
   def project do
     [
       app: :jira,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.8",
       name: "jira",
       description: @description,
